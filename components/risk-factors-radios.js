2417807cfb9ee15db8a7af45f17b50930e580ecb
Bastien <bzg@gnu.org>
2020-04-05 09:17:31 +0200

    components/risk-factors-radios.js: Fix immuno-suppressant question
    Fix https://github.com/etalab/covid19-orientation/issues/14

--- components/risk-factors-radios.js
+++ components/risk-factors-radios.js
@@ -188,7 +188,7 @@ function RiskFactors({handleRiskFactors}) {
                     {title: 'Je ne sais pas', value: 999}
                   ]}
                 >
-                  Prenez-vous un traitement immunosuppresseur ?
+                  Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).
                 </RadioChoices>
               </li>
             </ul>
