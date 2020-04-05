8dd017947f5f6142c18220e97088e558313e722d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2020-04-06 00:12:33 +0200

    fix: fix #47, skip temperature when no fever

--- pages/index.js
+++ pages/index.js
@@ -333,9 +333,10 @@ function App() {
       setIsFinish(true);
     }
 
-    // if (fever) {
-    //   nextStep = 5
-    // }
+    // when no fever, skip temperature
+    if (fever !== 1 && step === 4) {
+      nextStep = 5
+    }
 
     // when not tired, skip to cough
     if (tiredness === false) {
