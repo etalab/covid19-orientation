2897cb3ee56c045a168dad500371852aa79c0106
Bastien <bzg@gnu.org>
2020-04-07 10:21:52 +0200

    components/consent.js: Fix formatting

--- components/consent.js
+++ components/consent.js
@@ -6,17 +6,9 @@ function Consent({handleConsent}) {
     <article>
       <p>Ce questionnaire sert uniquement à vous orienter en fonction de votre état de santé et des symptômes que vous indiquez. L’avis qu'il fournit n’a pas de valeur médicale.</p>
 
-	<p>L’algorithme mis en oeuvre pour vous proposer une orientation est conforme à la{' '}
-          <a
-            target='new'
-            target='_top'
-            title='Accéder à la documentation dans un nouvel onglet'
-            href='https://delegation-numerique-en-sante.github.io/covid19-algorithme-orientation/index.html'
-          >
-          documentation publiée par le ministère des Solidarités et de la Santé
-          </a>{' '}
-	, qui référence aussi les autres solutions qui s’y conforment.  Vos réponses serviront à aider la recherche et la compréhension de l’épidémie de COVID 19.</p>
-	<p>Aucune information permettant de vous identifier, directement ou indirectement, n’est collectée. Voyez nos <a href='/about'>explications</a> à ce sujet.</p>
+      <p>L’algorithme mis en oeuvre pour vous proposer une orientation est conforme à la <a target='new' target='_top' title='Accéder à la documentation dans un nouvel onglet' href='https://delegation-numerique-en-sante.github.io/covid19-algorithme-orientation/index.html'>documentation publiée par le ministère des Solidarités et de la Santé</a>, qui référence aussi les autres solutions qui s’y conforment.  Vos réponses serviront à aider la recherche et la compréhension de l’épidémie de COVID 19.</p>
+
+      <p>Aucune information permettant de vous identifier, directement ou indirectement, n’est collectée. Voyez nos <a href='/about'>explications</a> à ce sujet.</p>
 
       <button className='mainbutton' type='submit' onClick={() => handleConsent(true)}>
         <span>J’ai compris</span> <i className='fas fa-check' aria-hidden='true' />
