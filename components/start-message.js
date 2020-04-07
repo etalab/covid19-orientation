160b16e592453845ab09b88aa8adbcb5b23ca7d4
Bastien <bzg@gnu.org>
2020-04-07 11:21:41 +0200

    Wording fixes

--- components/start-message.js
+++ components/start-message.js
@@ -5,7 +5,7 @@ function MessageAttente() {
     <article className='step' id='message-attente'>
       <div className='card message start-message'>
         <p className='primary-message'>
-          Vous pensez avoir des symptômes du Covid-19 et vous voulez savoir quoi faire ?
+          Vous pensez avoir des symptômes du Covid-19 et vous souhaitez savoir quel comportement adopter ?
           <br />
           Ce questionnaire est là pour vous orienter.
         </p>
