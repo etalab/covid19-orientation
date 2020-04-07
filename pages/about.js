a93cf55d3e6ddf7babedff1652e6d32d3cd04a6c
Bastien <bzg@gnu.org>
2020-04-07 10:13:52 +0200

    Stabilize the home and about page wording

--- pages/about.js
+++ pages/about.js
@@ -13,30 +13,18 @@ function About() {
       <StartMessage />
       <article>
         <p>
-          L’algorithme d’orientation COVID 19 est mis en œuvre conformément à la{' '}
-          <a
-            target='new'
-            target='_top'
-            title='Accéder à la documentation dans un nouvel onglet'
-            href='https://delegation-numerique-en-sante.github.io/covid19-algorithme-orientation/index.html'
-          >
-            documentation officielle
-          </a>{' '}
-          publiée par le ministère des Solidarités et de la Santé.
-        </p>
-        <p>
           Afin de rendre impossible toute réidentification des personnes
           concernées, les codes postaux ne sont retenus que s’ils correspondent
           à des communes de plus de 10 000 habitants. Pour les communes de moins
           de 10 000 habitants, un processus d’agrégation est mis en œuvre et
           seules les données agrégées sont conservées. Dans ce même but, seules
-          les tranches d’âge sont conservées et non l’âge exact.
+          les tranches d’âge et l'indice de masse corporelle sont conservés,
+          non l’âge exact ni la taille et le poids.
         </p>
 
         <div className='gouv-button-container'>
           <a className='gouv-button' href='/'>
-            <i className='fas fa-arrow-left' aria-hidden='true' /> Retour au
-            formulaire
+            <i className='fas fa-arrow-left' aria-hidden='true' /> Retour à la page précédente
           </a>
         </div>
       </article>
