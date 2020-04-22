0ea740d791bf09169689354da729258a4f2ba100
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-04-22 11:21:45 +0200

    Revert "(#110) 👙 ajout des min max pour les champs IMC"
    This reverts commit 6d458fd33c8f6a3696453f29c646ea32a153cc06.

--- components/respondent/imc.js
+++ components/respondent/imc.js
@@ -1,132 +1,70 @@
-import React, { useState, useCallback, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types';
+import React, {useState, useCallback} from 'react'
+import PropTypes from 'prop-types'
 
-const HEIGHT_MINMAX = {
-  min: 50,
-  max: 246,
-};
+function IMC({handleHeight, handleWeight}) {
+  const [weight, setWeight] = useState()
+  const [height, setHeight] = useState()
 
-const WEIGHT_MINMAX = {
-  min: 25,
-  max: 400,
-};
-
-function renderMandatoryFieldsMessage(msg) {
-  // Dupliquer depuis le composant
-  // risk-factor-radios
-  return (
-    <div className="message">
-      <div className="primary-message">{msg}</div>
-      <style jsx>{`
-        .primary-message {
-          margin: 0.5em 0;
-          text-align: center;
-          color: var(--alert-dark);
-        }
-      `}</style>
-    </div>
-  );
-}
-
-function IMC({ handleHeight, handleWeight }) {
-  const didMountRef = useRef(false);
-  const [weight, setWeight] = useState();
-  const [height, setHeight] = useState();
-  const [isWeightValid, setWeightIsValid] = useState(false);
-  const [isHeightValid, setHeightIsValid] = useState(false);
-
-  const handleSubmit = useCallback(
-    event => {
-      event.preventDefault();
-      handleHeight(height);
-      handleWeight(weight);
-    },
-    [height, weight, handleHeight, handleWeight]
-  );
-
-  useEffect(() => {
-    if (didMountRef.current) {
-      const weightIsValid =
-        weight >= WEIGHT_MINMAX.min && weight <= WEIGHT_MINMAX.max;
-      setWeightIsValid(weightIsValid);
-      const heightIsValid =
-        height >= HEIGHT_MINMAX.min && height <= HEIGHT_MINMAX.max;
-      setHeightIsValid(heightIsValid);
-    } else {
-      didMountRef.current = true;
-    }
-  });
+  const handleSubmit = useCallback(event => {
+    event.preventDefault()
+    handleHeight(height)
+    handleWeight(weight)
+  }, [height, weight, handleHeight, handleWeight])
 
   return (
-    <article className="step" id="imc-01">
-      <h2>
-        <i className="fas fa-weight" aria-hidden="true" />{' '}
-        <span>Indiquez votre poids et votre taille</span>
-      </h2>
-      <div className="card">
-        <form id="form-imc" onSubmit={handleSubmit}>
-          <div className="complement-infos">
-            <p>
-              Ces données sont utiles pour calculer votre indice de masse
-              corporelle.
-            </p>
+    <article className='step' id='imc-01'>
+      <h2><i className='fas fa-weight' aria-hidden='true' /> <span>Indiquez votre poids et votre taille</span></h2>
+      <div className='card'>
+        <form id='form-imc' onSubmit={handleSubmit}>
+          <div className='complement-infos'>
+            <p>Ces données sont utiles pour calculer votre indice de masse corporelle.</p>
             <ul>
               <li>
-                <label htmlFor="add-taille">Taille en centimètres * :</label>
+                <label htmlFor='add-taille'>Taille en centimètres * :</label>
                 <input
-                  type="number"
-                  required
-                  id="add-taille"
-                  min={HEIGHT_MINMAX.min}
-                  max={HEIGHT_MINMAX.max}
-                  placeholder="Par ex. 165"
+                  type='text'
+                  required pattern='[0-9]+'
+                  id='add-taille'
+                  value={height}
+                  data-check-number data-check-number-min='50'
+                  data-check-number-max='246'
+                  data-check-number-error='Ce nombre ne semble pas être une taille valide en centimètres.'
+                  placeholder='Par ex. 165'
+                  inputMode='numeric'
                   onChange={event => setHeight(event.target.value)}
                 />
               </li>
               <li>
-                <label htmlFor="add-poids">Poids en kilogrammes * :</label>
+                <label htmlFor='add-poids'>Poids en kilogrammes * :</label>
                 <input
-                  type="number"
-                  required
-                  id="add-poids"
-                  min={WEIGHT_MINMAX.min}
-                  max={WEIGHT_MINMAX.max}
-                  placeholder="Par ex. 70"
+                  type='text'
+                  required pattern='[0-9,.]+'
+                  id='add-poids'
+                  value={weight}
+                  data-check-number data-check-number-min='25'
+                  data-check-number-max='400'
+                  data-check-number-error='Ce nombre ne semble pas être un poids valide en kilogrammes.'
+                  placeholder='Par ex. 70'
+                  inputMode='decimal'
                   onChange={event => setWeight(event.target.value)}
                 />
               </li>
             </ul>
-            {didMountRef.current &&
-              !isWeightValid &&
-              weight !== undefined &&
-              renderMandatoryFieldsMessage(
-                'Ce nombre ne semble pas être un poids valide en kilogrammes.'
-              )}
+            <p>* La saisie de ces deux champs est obligatoire.</p>
           </div>
-          {didMountRef.current &&
-            !isHeightValid &&
-            height !== undefined &&
-            renderMandatoryFieldsMessage(
-              'Ce nombre ne semble pas être une taille valide en centimètres.'
-            )}
-          {isWeightValid && isHeightValid && (
-            <button className="mainbutton" type="submit">
-              <span>
-                Je mesure {height} cm et je pèse {weight} kilos.
-              </span>
-              <i className="fas fa-check" aria-hidden="true" />
-            </button>
-          )}
-          <p>* La saisie de ces deux champs est obligatoire.</p>
+          {height && weight && (
+            <button className='mainbutton' type='submit'>
+              <span>Je mesure {height} cm et je pèse {weight} kilos.</span><i className='fas fa-check' aria-hidden='true' />
+            </button>)}
         </form>
       </div>
     </article>
-  );
+  )
 }
 
 IMC.propTypes = {
   handleHeight: PropTypes.func.isRequired,
-  handleWeight: PropTypes.func.isRequired,
-};
+  handleWeight: PropTypes.func.isRequired
+}
 
-export default IMC;
+export default IMC
