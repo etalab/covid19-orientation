f4b300430f06fae1b43e8236e1a1639c13d50105
Bastien Guerry <bastien.guerry@data.gouv.fr>
2020-05-09 11:34:13 +0200

    Update explanations

--- pages/about.js
+++ pages/about.js
@@ -13,20 +13,20 @@ function About() {
       <StartMessage />
       <article>
         <p>
-          Afin de rendre impossible toute réidentification des personnes
-          concernées, les codes postaux ne sont retenus que s’ils correspondent
-          à des communes de plus de 10 000 habitants. Pour les communes de moins
-          de 10 000 habitants, un processus d’agrégation est mis en œuvre et
-          seules les données agrégées sont conservées. Dans ce même but, seules
-          les tranches d’âge et l’indice de masse corporelle sont conservés,
-          non l’âge exact ni la taille et le poids.
+          Si vous avez moins de 15 ans, un message est affiché
+          indiquant que l’application n’est pas adaptée à votre
+          situation, car l’algorithme servant à vous orienter n’a pas
+          été médicalement validé pour des populations de moins de 15
+          ans.  Dans ce cas, aucune donnée n’est collectée.
         </p>
+
         <p>
-          Si vous avez moins de 15 ans, un message est affiché indiquant que
-          l’application n’est pas adaptée à votre situation, car l’algorithme
-          servant à vous orienter n’a pas été médicalement validé pour des
-          populations de moins de 15 ans.  Dans ce cas, aucune donnée n'est
-	  collectée.
+          Si vous avez plus de 15 ans et que vous remplissez le
+          formulaire jusqu’à la fin, vos réponses sont envoyées vers
+          un serveur et restent anonymes : votre adresse IP n’est pas
+          collectée ; vos âges, poids et tailles exacts ne sont pas
+          collectés, seuls le sont votre tranche d’âge et votre indice
+          de masse corporelle.
         </p>
 
         <div className='gouv-button-container'>
